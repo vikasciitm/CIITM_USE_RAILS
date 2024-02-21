c2a9ff7acf549c13121202a1bfc6c0aa14dd534d
vikasciitm <geniusmv38@gmail.com>
2024-02-21 21:39:50 +0530

    v 1.0

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root "home#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # students roots start
+  get "students/new"
+end
